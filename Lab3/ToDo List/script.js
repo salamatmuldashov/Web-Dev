401a993556872d12f6ac10370f7b26fba0899151
Salamat Muldashov <s_muldashov@kbtu.kz>
2023-03-08 19:05:44 +0600

    commit

--- Lab3/ToDo List/script.js
+++ Lab3/ToDo List/script.js
@@ -2,18 +2,27 @@ const todoInput = document.getElementById("todoInput");
 const addButton = document.getElementById("addButton");
 const todoList = document.getElementById("todoList");
 const deleteButton = document.getElementById("deleteButton");
+const todoInputTime = document.getElementById("todoInputTime");
 
 function addTodo() {
-  const li = document.createElement("li");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.addEventListener("click", toggleDone);
-  const label = document.createElement("label");
-  label.textContent = todoInput.value;
-  li.appendChild(checkbox);
-  li.appendChild(label);
-  todoList.appendChild(li);
-  todoInput.value = "";
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("click", toggleDone);
+    const label = document.createElement("label");
+    label.textContent = todoInput.value;
+    const time = document.createElement("input")
+    time.type = "date";
+    time.value = todoInputTime.value;
+
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.appendChild(time);
+
+    todoList.appendChild(li);
+  
+    todoInput.value = "";
+  
 }
 
 function toggleDone(event) {
@@ -39,10 +48,11 @@ function deleteTodo() {
 
 
 addButton.addEventListener("click", addTodo);
-document.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    addTodo();
-  }
-});
+// document.addEventListener("keydown", function(event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     addTodo();
+//   }
+// });
 deleteButton.addEventListener("click", deleteTodo);
+
